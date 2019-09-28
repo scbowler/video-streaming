94ee0ae54df2e28ae302f0848a91518612a4b866
Scott Bowler <scott.bowler@learningfuze.com>
2019-09-28 00:23:03 +0000

    Setup initial oauth sign in, working

--- index.js
+++ index.js
@@ -1,27 +1,50 @@
 const express = require('express');
 const { resolve } = require('path');
+const axios = require('axios');
 const PORT = process.env.PORT || 9000;
+const googleConfig = require('./config/google');
 
 const app = express();
 
 app.use(express.static(resolve(__dirname, 'public')));
 
 app.get('/auth', (req, res) => {
-    const url = `https://accounts.google.com/o/oauth2/auth?
-        client_id=&
-        redirect_uri=http%3A%2F%2Flocalhost%2Foauth2callback&
-        scope=https://www.googleapis.com/auth/youtube&
-        response_type=code&
-        access_type=offline`;
+    const url = `https://accounts.google.com/o/oauth2/auth?client_id=${googleConfig.clientId}&redirect_uri=http%3A%2F%2Flocalhost%3A9000%2Fauth%2Fgoogle&scope=https://www.googleapis.com/auth/youtube https://www.googleapis.com/auth/youtube.upload&response_type=code&access_type=offline`;
 
+    console.log('URL:', url);
 
     res.redirect(url);
 });
 
-app.get('/auth/google', (req, res) => {
+app.get('/auth/google', async (req, res) => {
+    const { code } = req.query;
+    
+    try {
+        const resp = await axios.post('https://accounts.google.com/o/oauth2/token', postData({
+            code,
+            client_id: googleConfig.clientId,
+            client_secret: googleConfig.clientSecret,
+            redirect_uri: 'http://localhost:9000/auth/google',
+            grant_type: 'authorization_code'
+        }), {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        });
+
+        console.log('Response:', resp);
+
+
+        res.send('Where will this go?');
+    } catch(err){
+        console.log('Auth Error:', err);
+        res.status(500).send('It broke');
+    }
+});
+
+app.get('/auth/final', async (req, res) => {
     console.log('Query:', req.query);
     console.log('Headers:', req.headers);
-    console.log('Body:', req.body);
 
     res.redirect('/');
 });
@@ -29,3 +52,13 @@ app.get('/auth/google', (req, res) => {
 app.listen(PORT, () => {
     console.log('Server Listening @ localhost:%d', PORT);
 });
+
+function postData(obj){
+    const params = new URLSearchParams();
+
+    for([k, v] of Object.entries(obj)){
+        params.append(k, v);
+    }
+
+    return params;
+}
